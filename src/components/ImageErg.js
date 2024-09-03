c223ef2f6bd2de56e9f387e83cba22fec1bd507a
leerlandais <leerlandais@gmail.com>
2024-09-03 11:26:46 +0100

    enterprise page finished

---
+++ src/components/ImageErg.js
@@ -0,0 +1,9 @@
+function ImageErg() {
+    return (
+        <div id="entreimg">
+            <a href="https://wiki.erg.be/m/#Bienvenue_%C3%A0_l%E2%80%99erg" target="_blank"><img src="../images/ERG.png" /></a>
+        </div>
+    )
+}
+
+export default ImageErg

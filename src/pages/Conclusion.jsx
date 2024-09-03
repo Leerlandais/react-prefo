c144715de6dee1bd8ce71135148ff2976bd63ef8
leerlandais <leerlandais@gmail.com>
2024-09-03 10:31:22 +0100

    finished conclusion page

--- src/pages/Conclusion.jsx
+++ src/pages/Conclusion.jsx
@@ -1,6 +1,27 @@
 
 function Conclusion() {
-    return("Conclusion");
+    return(
+        <>
+        <h2>Conclusion</h2>
+
+   <div class="conc">
+        <p>
+            Une des plus grandes difficultés auxquelles j'ai été confronté pendant la Préformation a été le passage de 'Inconscience des Incompetences' à 'Connaissance des Incompetences'. En apprenant essentiellement HTML et CSS à partir de zéro, j'ai souvent été confronté à un dilemme lors de la création du site. En apprenant de nouvelles techniques, il était souvent plus facile de reconstruire le site à partir de zéro plutôt que de tout réécrire, car cela signifiait beaucoup de code redondant (surtout en CSS).<br />
+            Heureusement, la première étape pour résoudre un problème est d'en être conscient. Gardant cela à l'esprit lorsque j'ai commencé la construction finale du site, je me suis assuré de tout organiser de manière à pouvoir facilement repérer et améliorer les éléments chaque fois que je découvrais une nouvelle technique qui nécessitait de modifier une grande quantité de code. <br />
+            Je suis assez satisfait de mon développement au cours de ces dernières semaines, ce qui m'a vraiment donné l'envie de continuer. Parce que le Front-End n'est pas mon point fort (je n'ai jamais été très artistique), j'ai décidé de me concentrer sur la fonctionnalité et la cohérence de mon site. J'ai hâte d'apprendre JavaScript pour pouvoir améliorer davantage mon site, et j'attends avec impatience la suite du cours et l'apprentissage de la partie Back-End du développement web.
+        </p>
+        <p>
+            Probablement l'aspect le plus agréable des dernières semaines a été le sentiment de communauté qui s'est développé entre nous, les étudiants. C'était un plaisir de voir tout le monde s'entraider au fur et à mesure des semaines et j'ai hâte de travailler avec eux pendant la Formation Qualifiante.
+        </p>
+        <p>
+            Je vais conclure en disant merci à tous ceux de C2FM. Leur bonheur, leur soutien et leur accueil ont été grandement appréciés!
+        </p>
+        <p id="lastnote">
+            Tout petit dernier mot: (J'ai plus ou moins (plus plus que moins) terminé mon site aujourd'hui, jeudi, et j'ai passé la majeure partie de la journée à aider les autres avec n'importe quel problème qu'ils rencontraient (parfois avec succès, parfois sans, mais toujours avec progrès !). J'ai remarqué plus tôt qu'un de mes collègues avait une erreur avec les liens sur l'une de ses pages. J'ai réussi à utiliser Github pour cloner ses fichiers, corriger l'erreur et lui proposer la solution. J'ai terminé cela juste avant que ma femme rentre à la maison, pour me trouver en train de danser et de chanter : "ça a marché, ça a marché". Maintenant, j'attends avec encore plus d'impatience l'avenir !)
+        </p>
+    </div>
+        </>
+    );
 }
 
 export default Conclusion;

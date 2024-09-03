1274d1c7565f503ac659042053cf5ebba783cc20
leerlandais <leerlandais@gmail.com>
2024-09-03 11:37:59 +0100

    musuem page finished

---
+++ src/components/ImageExpo1.js
@@ -0,0 +1,9 @@
+function ImageExpo1() {
+    return (
+        <div id="expoimg">
+            <img src="./images/thumb/photo37.jpg" />
+        </div>
+    )
+}
+
+export default ImageExpo1
---
+++ src/components/ImageExpo2.js
@@ -0,0 +1,9 @@
+function ImageExpo2() {
+    return (
+        <div id="expoimg2">
+            <img src="./images/thumb/photo45.jpg" />
+        </div>
+    )
+}
+
+export default ImageExpo2
